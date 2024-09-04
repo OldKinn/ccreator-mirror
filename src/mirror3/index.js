1d7c475221bab72ba4c3feac85c56dd57fe36d89
Leon <leonooo@qq.com>
2024-09-04 14:35:47 +0800

    删除日志输出

--- src/mirror3/index.js
+++ src/mirror3/index.js
@@ -1,9 +1,5 @@
 import * as mirror from './mirror';
 
-const version = import.meta.env.VITE_PKG_VERSION || 'x.x.x';
-const time = import.meta.env.VITE_TIMESTAMP || 'xxxx-xx-xx xx:xx:xx';
-mirror.debug(`当前版本:${version}, 编译时间:${time}`, '@ccreator/mirror', 'orange');
-
 export { Provider, connect } from 'react-redux';
 
 export * from './mirror';
